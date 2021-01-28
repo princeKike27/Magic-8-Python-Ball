d265e3457df749e89c8b148d93ae36732f324c59
princeKike27 <70545564+princeKike27@users.noreply.github.com>
2021-01-28 15:19:40 -0500

    Update magic_ball.py

--- magic_ball.py
+++ magic_ball.py
@@ -1,9 +1,10 @@
 """
 MAGIC 8-PYTHON-BALL
 
-The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
-It was invented in 1950 by Albert C. Carter and Abe Bookman and is currently manufactured by Mattel.
-The user asks a yes–no question to the ball, then turns it over to reveal an answer in a window on the ball. 
+For this project I created a Python implementation of the *Magic 8-Ball* game. The Magic 8-Ball is a popular 
+toy developed in the 1950s for fortune-telling or advice seeking. The user asks a yes–no question to the ball, 
+then turns it over to reveal an answer in a window on the ball. It was invented in 1950 by Albert C. Carter a
+nd Abe Bookman and is currently manufactured by Mattel.
 """
 from random import choice
 
